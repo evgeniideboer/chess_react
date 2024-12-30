cc8403afdeda9f788be9b0265ce4ca8d30e64f5a
Evgenii de-Bur <evd8@inbox.ru>
2024-12-31 02:16:08 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../src/App.css'
+import BoardComponent from "./components/BoardComponent";
+
+const App = () => {
+  return <div className="app">
+   <BoardComponent/>
+    </div>;
+};
+
+export default App;
